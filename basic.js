2af3c472980713120216be6c2d674bab9d804bb1
Lokesh-Amara <72940205+Lokesh-Amara@users.noreply.github.com>
2020-11-01 23:12:35 +0530

    Update basic.js

--- basic.js
+++ basic.js
@@ -5,9 +5,8 @@ request.open('GET', 'https://restcountries.eu/rest/v2/all', true);
 request.send();
 
 request.onload = function() {
-    console.log("abc");
     var data = JSON.parse(this.response);
-    //console.log(data)
+    
     for( const i of data){
         console.log(i.flag);
     }
